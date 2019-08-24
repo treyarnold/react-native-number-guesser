af1c163d8e6e32bfbb50590ebc5ab95f79a8cebd
Trey Arnold <arnold.trey80@gmail.com>
2019-08-24 16:01:39 -0500

    created header and card UI elements

---
+++ template.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ComponentName = (props) => {
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ComponentName;
