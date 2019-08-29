ed3e3160589eaed7affd8e34f4716e85f1ab0086
Trey Arnold <arnold.trey80@gmail.com>
2019-08-29 08:01:50 -0500

    game screen and guessing functionality done

---
+++ screens/GameScreen.js
@@ -0,0 +1,74 @@
+import React, { useState, useRef, useEffect } from "react";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+
+  useEffect(() => {
+    if (currentGuess === props.userChoice) {
+
+    }
+  });
+
+  const nextGuesssHandler = (direction) => {
+    if ((direction === "lower" && currentGuess < props.userChoice) ||
+      (direction === "greater" && currentGuess > props.userChoice)) {
+      Alert.alert("You checting fuck", "You know that is wrong",
+        [{ text: "Sorry!", style: "cancel" }]);
+      console.log("lie");
+      return;
+    }
+    if (direction === "lower") {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+    setCurrentGuess(nextNumber);
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={() => { nextGuesssHandler("lower") }} />
+        <Button title="GREATER" onPress={() => { nextGuesssHandler("greater") }} />
+      </Card>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  }
+});
+
+export default GameScreen;
